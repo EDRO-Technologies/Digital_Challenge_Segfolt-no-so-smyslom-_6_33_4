84c645b926443b6f4e19d6bca7d4cb0472650a30
Your Name <you@example.com>
2025-11-23 06:22:30 +0300

    123

---
+++ myapp/.svelte-kit/non-ambient.d.ts
@@ -0,0 +1,45 @@
+
+// this file is generated — do not edit it
+
+
+declare module "svelte/elements" {
+	export interface HTMLAttributes<T> {
+		'data-sveltekit-keepfocus'?: true | '' | 'off' | undefined | null;
+		'data-sveltekit-noscroll'?: true | '' | 'off' | undefined | null;
+		'data-sveltekit-preload-code'?:
+			| true
+			| ''
+			| 'eager'
+			| 'viewport'
+			| 'hover'
+			| 'tap'
+			| 'off'
+			| undefined
+			| null;
+		'data-sveltekit-preload-data'?: true | '' | 'hover' | 'tap' | 'off' | undefined | null;
+		'data-sveltekit-reload'?: true | '' | 'off' | undefined | null;
+		'data-sveltekit-replacestate'?: true | '' | 'off' | undefined | null;
+	}
+}
+
+export {};
+
+
+declare module "$app/types" {
+	export interface AppTypes {
+		RouteId(): "/" | "/calendar" | "/login" | "/notifications" | "/schedule";
+		RouteParams(): {
+			
+		};
+		LayoutParams(): {
+			"/": Record<string, never>;
+			"/calendar": Record<string, never>;
+			"/login": Record<string, never>;
+			"/notifications": Record<string, never>;
+			"/schedule": Record<string, never>
+		};
+		Pathname(): "/" | "/calendar" | "/calendar/" | "/login" | "/login/" | "/notifications" | "/notifications/" | "/schedule" | "/schedule/";
+		ResolvedPathname(): `${"" | `/${string}`}${ReturnType<AppTypes['Pathname']>}`;
+		Asset(): string & {};
+	}
+}
